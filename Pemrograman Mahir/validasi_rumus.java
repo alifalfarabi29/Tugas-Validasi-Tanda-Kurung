4e06b1d72c103db534cbc6fb4a65c7bfdace9421
alifalfarabi29 <85091252+alifalfarabi29@users.noreply.github.com>
2021-10-06 17:01:38 +0700

    Update validasi_rumus.java

--- Pemrograman Mahir/validasi_rumus.java
+++ Pemrograman Mahir/validasi_rumus.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 
 public class validasi_rumus {
     public static void main(String[] args) {
-        StackGeneric<String> stack = new StackGeneric<>(12);
+        StackGeneric<String> stack = new StackGeneric<>(15);
         String input = "f(x)=(x-3)(x+9)";
         Scanner inp = new Scanner(input);
 
         inp.useDelimiter("");
 
-        for (int i = 0; i < 12; i++) {
+        for (int i = 0; i < 15; i++) {
             String Char = inp.next();
             if(Char.equals("(")) {
                 stack.push("(");
